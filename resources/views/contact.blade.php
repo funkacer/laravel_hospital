c2a9f083ec190b91498c523ec62a892c8d557129
funkacer <funkacer@gmail.com>
2023-09-30 09:01:34 +0200

    Init

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kontakty</title>
+</head>
+<body>
+    <h1>Kontakty</h1>
+    <ul>
+        <li>Primář</li>
+        <li>Sestra</li>
+    </ul>
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $status = ['zdravy', 'nemocny', 'mrtvy'];
+    $variables = ['name' => 'Jirka',
+        'age' => 49,
+        'appointments' => ['28.10.2023', '10.11.2023', '31.12.2024'],
+        'status' => $status[random_int(0, count($status)-1)]
+    ];
+    return view('welcome', $variables);
+});
+
+Route::get('/kontakty', function () {
+    return view('contact');
+})->name('contactRoute');
